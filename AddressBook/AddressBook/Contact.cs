44e6c4da7bde7b72dc5823d80692689cb37d89ed
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-22 19:16:07 +0530

    [Rohit]ADD: Edit a Contacts in Address Book

--- AddressBook/AddressBook/Contact.cs
+++ AddressBook/AddressBook/Contact.cs
@@ -6,7 +6,7 @@ namespace AddressBook
 {
     class Contact
     {
-        //Add New Contacts
+        //Contacts
         public Contact(string firstname, string lastname, string address, string city, string state, string zipcode, string phonenumber, string email)
         {
             FirstName = firstname;
