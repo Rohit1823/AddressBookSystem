511ab7f8e2e67cbbbc11f0a2754a2dce73ba2955
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-01 12:07:53 +0530

    [Rohit]ADD: Sort entries of person in an alphabetically order

--- AddressBook/AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AddressBook
@@ -191,6 +192,14 @@ namespace AddressBook
 
             }
             Console.WriteLine("\nNumber of Person found in " + cityState + " are " + countPerson);
+        }
+        public void SortByPersonName()
+        {
+            Dictionary<string, Contact> sortList = DictName.OrderBy(x => x.Value.FirstName).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var element in sortList)
+            {
+                Console.WriteLine(element.Value.FirstName + " " + element.Value.LastName + " " + element.Value.PhoneNumber);
+            }
         }
     }
 }
